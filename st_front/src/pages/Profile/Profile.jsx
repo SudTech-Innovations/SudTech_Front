dfbc715ac41a1e55db42a92604b117d9403e2fa5
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-07-03 11:47:45 +0200

    wip theme

--- st_front/src/pages/Profile/Profile.jsx
+++ st_front/src/pages/Profile/Profile.jsx
@@ -4,6 +4,7 @@ import { UserContext } from "../../models/utils/context/UserContext";
 export default function Profile() {
   const { checkToken } = useContext(UserContext);
   const [userId, setUserId] = useState(null);
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
     const fetchUserId = async () => {
@@ -16,10 +17,38 @@ export default function Profile() {
     fetchUserId();
   }, [checkToken]);
 
+  const handleThemeChange = async (event) => {
+    const newTheme = event.target.value;
+    setTheme(newTheme);
+
+    try {
+      const response = await fetch(`/api/user`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ theme: newTheme }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to update user theme");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <h1>Profile</h1>
       <p>Utilisateur : {userId} </p>
+      <label>
+        Thème :
+        <select value={theme} onChange={handleThemeChange}>
+          <option value="light">Clair</option>
+          <option value="dark">Sombre</option>
+        </select>
+      </label>
     </>
   );
 }
