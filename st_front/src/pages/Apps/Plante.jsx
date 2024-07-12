dd1980c003223f9d6c668594c2de06dde5556eba
Nicolas Vinckier <nicolas.vinckier05@gmail.com>
2024-07-12 09:29:46 +0200

    css

--- st_front/src/pages/Apps/Plante.jsx
+++ st_front/src/pages/Apps/Plante.jsx
@@ -4,6 +4,27 @@ import Cookies from "js-cookie";
 
 export default function AppPlante() {
   const [plantes, setPlantes] = useState([]);
+  const labelMapping = {
+    commonName: "Nom commun",
+    scientificName: "Nom scientifique",
+    family: "Famille",
+    genus: "Genre",
+    species: "Espèce",
+    plantType: "Type de plante",
+    origin: "Origine",
+    hardinessZone: "Zone de rusticité",
+    maxHeight: "Hauteur maximale en cm",
+    maxWidth: "Largeur maximale en cm",
+    lightRequirement: "Besoin en lumière",
+    soilPreference: "Préférence de sol",
+    waterRequirement: "Besoin en eau",
+    floweringPeriod: "Période de floraison",
+    flowerColor: "Couleur de fleur",
+    foliageType: "Type de feuillage",
+    usage: "Utilisation",
+    description: "Description",
+    imageUrl: "URL de l'image",
+  };
 
   useEffect(() => {
     const token = Cookies.get("token");
@@ -32,26 +53,30 @@ export default function AppPlante() {
       <div id="pagePlantes">
         <h1>Page Plantes</h1>
 
-        {plantes.map((plant) => (
-          <div key={plant.id} className="plant">
-            {Object.entries(plant).map(([key, value]) => {
-              if (value !== null && value !== "") {
-                return (
-                  <div key={key} className={"plant-" + key}>
-                    <span className="detail-label">{key}:</span>
-                    <span className="detail-value">{value}</span>
-                  </div>
-                );
-              }
-              return null;
-            })}
-            <img
-              src={plant.imageUrl}
-              alt={plant.commonName}
-              className="plant-image"
-            />
-          </div>
-        ))}
+        <div className="allPlants">
+          {plantes.map((plant) => (
+            <div key={plant.id} className="plant">
+              {Object.entries(plant).map(([key, value]) => {
+                if (value !== null && value !== "") {
+                  return (
+                    <div key={key} className={"plant-" + key}>
+                      <span className="detail-label">
+                        {labelMapping[key] || key}:
+                      </span>
+                      <span className="detail-value">{value}</span>
+                    </div>
+                  );
+                }
+                return null;
+              })}
+              <img
+                src={plant.imageUrl}
+                alt={plant.commonName}
+                className="plant-image"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
